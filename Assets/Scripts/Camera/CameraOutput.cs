0193131b6ae852a74fb3ce9a09b0af0d075a5a1a
pawel.gogolak <pafffel@cluain.pl>
2020-07-02 22:44:07 +0200

    Added sub target areas only for CameraOutput.cs now / TODO: take histogram from multiple sub target areas

--- Assets/Scripts/Camera/CameraOutput.cs
+++ Assets/Scripts/Camera/CameraOutput.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace OpenCvSharp.Demo {
     public class CameraOutput : MonoBehaviour {
-        [Header("Target Area")] public Rect targetArea;
+        [Header("Target Area")] public List<Rect> subTargetAreas = new List<Rect>();
+        public int subTargetAreasColumnCount = 3;
+        public int subTargetAreasRowCount = 3;
+
+        public int subTargetAreasCount {
+            get => subTargetAreasColumnCount * subTargetAreasRowCount;
+        }
+
+        [Range(1, 100)] public int subTargetAreaSize = 10;
+
+        public Rect targetArea;
         public Vector2 targetAreaPercentSize = new Vector2(0.1f, 0.1f);
         public Color targetAreaColor;
 
@@ -31,10 +42,35 @@ namespace OpenCvSharp.Demo {
             float targetAreaHeight = cameraTexture.height * targetAreaPercentSize.y;
             targetArea = new Rect(new Point((cameraTexture.width / 2) - targetAreaWidth * 0.5, (cameraTexture.height / 2) - targetAreaHeight * 0.5),
                 new Size(cameraTexture.width * targetAreaPercentSize.x, cameraTexture.height * targetAreaPercentSize.y));
+
+            int subAreaStepX = Mathf.RoundToInt(targetAreaWidth / subTargetAreasColumnCount);
+            int subAreaStepY = Mathf.RoundToInt(targetAreaHeight / subTargetAreasRowCount);
+
+            Point subAreaStartingPosition = targetArea.TopLeft;
+            subAreaStartingPosition.X += subAreaStepX / 2;
+            subAreaStartingPosition.Y += subAreaStepY / 2;
+            Size subAreaPositionOffset = new Size(subTargetAreaSize / 2, subTargetAreaSize / 2);
+            for (int i = 0; i < subTargetAreasCount; i++) {
+                Rect subAreaRect = new Rect();
+                subAreaRect.Size = new Size(subTargetAreaSize, subTargetAreaSize);
+
+                int rowIndex = i / subTargetAreasColumnCount;
+                int columnIndex = i % subTargetAreasColumnCount;
+
+                subAreaRect.X = subAreaStartingPosition.X + subAreaStepX * columnIndex - subAreaPositionOffset.Width;
+                subAreaRect.Y = subAreaStartingPosition.Y + subAreaStepY * rowIndex - subAreaPositionOffset.Height;
+
+                subTargetAreas.Add(subAreaRect);
+            }
         }
 
         private Mat addTargetAreaRect(Mat inputMat) {
+            for (int i = 0; i < subTargetAreas.Count; i++) {
+                Cv2.Rectangle(inputMat, subTargetAreas[i].TopLeft, subTargetAreas[i].BottomRight, new Scalar(0, 255, 0), 5);
+            }
+
             Cv2.Rectangle(inputMat, targetArea.TopLeft, targetArea.BottomRight, new Scalar(0, 255, 0), 5);
+
             return inputMat;
         }
     }
